6951f4b66250de88aa550605c29ba121b5b5ee6a
Dave Neuman <neuman@apache.org>
2017-01-05 07:11:50 -0700

    use -P instead of -p

--- traffic_router/build/build_rpm.sh
+++ traffic_router/build/build_rpm.sh
@@ -57,7 +57,7 @@ function buildRpmTrafficRouter () {
 
 	cd "$TR_DIR" || { echo "Could not cd to $TR_DIR: $?"; exit 1; }
 	export BUILD_NUMBER=${BUILD_NUMBER:-$(getBuildNumber)}
-	mvn -P rpm-build -p neustar -Dmaven.test.skip=true -DminimumTPS=1 clean package ||  \
+	mvn -P rpm-build -P neustar -Dmaven.test.skip=true -DminimumTPS=1 clean package ||  \
 		{ echo "RPM BUILD FAILED: $?"; exit 1; }
 
 	local rpm=$(find -name \*.rpm)
